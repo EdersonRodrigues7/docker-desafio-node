ffbd88e7f0960aa611f0c33a8840d9f096dcb82e
Ederson Rodrigues <ederson.rodrigues777@gmail.com>
2025-02-11 21:23:54 -0300

    fix(main): Fix database on init.sql

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS fullcycle;
-USE fullcycle;
+USE teste;
 CREATE TABLE IF NOT EXISTS people (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, PRIMARY KEY (id));
 INSERT INTO people (name) VALUES ("Fulano"), ("Cicrano"), ("Beltrano");
